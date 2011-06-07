d850575bd63b153b671bebacd2164f7f62c8ae62
tell-k <ffk2005@gmail.com>
2011-06-07 15:34:33 +0900

    update document

--- work/upload.sh
+++ work/upload.sh
@@ -4,9 +4,11 @@ git pull
 git add ./
 git commit -a -m 'update document' 
 git push origin gh-pages
-echo "push gh-pages"
+echo "push gh-pages -------------------"
 cd ../
+git pull
+git add ./
 git commit -a -m 'update document' 
 git push origin master
-echo "push master"
+echo "push master  --------------------"
 
