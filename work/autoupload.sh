b690f31234c41dd559b394801844309f5947733b
tell-k <ffk2005@gmail.com>
2011-06-07 17:20:07 +0900

    update document

--- work/autoupload.sh
+++ work/autoupload.sh
@@ -1,3 +1,2 @@
 #watchmedo shell-command --patterns="*.rst" --recursive --command='sh upload.sh' ./ > /dev/null 2>&1 & 
-watchmedo shell-command --patterns="conf.py" --recursive --command='sh upload.sh'
 
