6af8a000af0000e102ae37942ad6eacb85958784
tell-k <ffk2005@gmail.com>
2011-06-07 17:14:35 +0900

    update document

---
+++ work/autoupload.sh
@@ -0,0 +1,3 @@
+#watchmedo shell-command --patterns="*.rst" --recursive --command='sh upload.sh' ./ > /dev/null 2>&1 & 
+watchmedo shell-command --patterns="*.rst" --recursive --command='sh upload.sh'
+
