a484dfe9816d698d9b6a0642b72e7ae3b3e56789
tell-k <ffk2005@gmail.com>
2011-06-07 15:32:03 +0900

    update document

---
+++ work/upload.sh
@@ -0,0 +1,12 @@
+CURRENT_PATH=$(cd $(dirname $0) && pwd)
+cd $CURRENT_PATH/../
+git pull
+git add ./
+git commit -a -m 'update document' 
+git push origin gh-pages
+echo "push gh-pages"
+cd ../
+git commit -a -m 'update document' 
+git push origin master
+echo "push master"
+
