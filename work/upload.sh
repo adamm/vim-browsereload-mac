52e01b2151da8eb7314f6578d3a2ad1a67f92907
tell-k <ffk2005@gmail.com>
2011-06-07 16:44:49 +0900

    update document

--- work/upload.sh
+++ work/upload.sh
@@ -1,14 +1,18 @@
 CURRENT_PATH=$(cd $(dirname $0) && pwd)
 cd $CURRENT_PATH/../
-git pull
+echo "make docs ----------------------------------------------"
+make html
+cp -pr ./_build/html/* ../
+git pull --rebase
 git add ./
+echo "commit & push gh-pages ---------------------------------"
 git commit -a -m 'update document' 
 git push origin gh-pages
-echo "push gh-pages ---------------------------------"
 cd ../
-git pull
+git pull --rebase
 git add ./
+echo "commit & push master -----------------------------------"
 git commit -a -m 'update document' 
 git push origin master
-echo "push master -----------------------------------"
-
+echo "finish upload ------------------------------------------"
+osascript -e 'tell application "Google Chrome" to reload active tab of window 1'
