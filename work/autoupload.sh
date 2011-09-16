a5a0cfe2d96a9f9f33c7867e693d002bab2f2805
tell-k <ffk2005@gmail.com>
2011-09-16 20:44:37 +0900

    commit

--- work/autoupload.sh
+++ work/autoupload.sh
@@ -1,2 +1,2 @@
-#watchmedo shell-command --patterns="*.rst" --recursive --command='sh upload.sh' ./ > /dev/null 2>&1 & 
+watchmedo shell-command --patterns="*.rst" --recursive --command='sh upload.sh' ./ > /dev/null 2>&1 & 
 
